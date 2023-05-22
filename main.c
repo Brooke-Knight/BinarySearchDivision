bebdb7097517c4fa61d1372af699dff3c80fe2ba
Brooke-Knight <134266294+Brooke-Knight@users.noreply.github.com>
2023-05-22 05:58:39 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,33 @@
-float division(float dividend, float divisor)
+double unsigned_divide(double dividend, double divisor)
 {
-    float lno = 0;
-    float lt = 0;
-    float ct = 1;
+    double lno = 0;
+    double lt  = 0;
+    double ct  = 1;
+
     while (ct * divisor != dividend)
     {
         if (ct * divisor < dividend)
         {
             lno = ct;
-            lt = ct;
-            ct = ct * 2;
+            lt  = ct;
+            ct  = ct * 2;
         }
         else
         {
             lt = ct;
             ct = lno + (0.5 * (ct - lno));
         }
-    }  
+    }
     return ct;
+}
+
+double signed_divide(double dividend, double divisor)
+{
+    if (dividend == 0) return 0;
+    if (divisor  == 0) return 0;
+    if (divisor  == 1) return dividend;
+    if (dividend > 0 && divisor > 0) return  unsigned_divide( dividend,  divisor);
+    if (dividend < 0 && divisor < 0) return -unsigned_divide(-dividend, -divisor);
+    if (dividend < 0 && divisor > 0) return -unsigned_divide(-dividend,  divisor);
+    if (dividend > 0 && divisor < 0) return -unsigned_divide( dividend, -divisor);
 }
